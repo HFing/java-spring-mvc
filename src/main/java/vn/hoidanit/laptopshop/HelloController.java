890ae9f8d873417efb9c8d6aca75d40ea6e80f4b
Nguyễn Thái Hoàng <thaihoangon.on@gmail.com>
2024-07-17 23:41:05 +0700

    update security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World Co Len !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Page User !";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Page Admin !";
     }
 }
