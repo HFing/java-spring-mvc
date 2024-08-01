ee783e15443e1332b66fc91679864c4d4aaa9e2b
Nguyễn Thái Hoàng <thaihoangon.on@gmail.com>
2024-08-01 21:46:23 +0700

    update #100

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -1,14 +1,33 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import vn.hoidanit.laptopshop.domain.Product;
 
 @Controller
 public class ProductController {
     @GetMapping("/admin/product")
     public String getDashboard() {
         return "admin/product/show";
+    }
+
+    @GetMapping("/admin/product/create")
+    public String getCreateProduct(Model model) {
+        model.addAttribute("newProduct", new Product());
+        return "admin/product/create";
+    }
+
+    @PostMapping(value = "/admin/product/create")
+    public String createProductPage(Model model, @ModelAttribute("newUser") Product newProduct,
+            @RequestParam("avatarFile") MultipartFile file) {
+
+        return "redirect:/admin/product";
     }
 
 }
